fa552ef29e4ab6f01ded2fc8268231f8528edff5
hoover13 <hoovermtk25@gmail.com>
2024-02-20 22:01:41 -0500

    Initial Commit

---
+++ Myanmar Honorable Women/Myanmar_Honorable_WomenApp.swift
@@ -0,0 +1,17 @@
+//
+//  Myanmar_Honorable_WomenApp.swift
+//  Myanmar Honorable Women
+//
+//  Created by Min Thu Khine on 2/20/24.
+//
+
+import SwiftUI
+
+@main
+struct Myanmar_Honorable_WomenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
